6941a362b36f4b55db683a982094ecbaed6d5d4a
Pankaj kr <pankajkmr9931@gmail.com>
2025-01-26 16:43:55 +0530

    FInal deployment

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+
+var typeData = new Typed(".role", {
+  strings: [
+    "Pankaj Kumar",
+    "a Frontend Web Developer",
+    "a Coder",
+  ],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+});
+
